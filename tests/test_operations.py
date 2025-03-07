5c5a813d544db9f5091c92402d3d9df7509d1426
d3v07 <trivedidev16.com>
2025-03-06 20:28:58 -0500

    test: update existing tests for new implementation

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,16 +3,16 @@ from calculator.operations import add, multiply, subtract, divide
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(1,1) == 2
+    assert add(2,2) == 4
 
 def test_subtraction():
     '''Test that addition function works '''    
-    assert subtract(1,1) == 0
+    assert subtract(4,4) == 0
 
 def test_multiplication():
     '''Test that multiply works'''
-    assert multiply(1,1) == 1
+    assert multiply(2,2) == 4
 
 def test_division():
     '''Test division'''
-    assert divide(1,1) == 1
+    assert divide(4,1) == 4
